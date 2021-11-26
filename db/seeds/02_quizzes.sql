2656779a8d675ecb853b5ee06edc7558aaa3efd2
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-26 23:57:20 +0000

    added titles

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,4 +1,4 @@
-INSERT INTO quizzes (creator_id, status, category, cover_image_url) VALUES(1, TRUE, 'Geography', 'https://i2.wp.com/educationblog.oup.com/wp-content/uploads/2017/02/oup_22335.jpg?w=2553&ssl=1');
-INSERT INTO quizzes (creator_id, status, category) VALUES(2, FALSE, 'Astronomy', 'https://astronomy.com/-/media/Images/News%20and%20Observing/News/2020/09/Outlooknbehqb2r.png?mw=600');
-INSERT INTO quizzes (creator_id, status, category) VALUES(1, TRUE, 'Math', 'https://bhi61nm2cr3mkdgk1dtaov18-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/beautiful-math-1.jpg');
+INSERT INTO quizzes (creator_id, title, isPublic, category, cover_image_url) VALUES(1, 'Super fun awesome quiz' TRUE, 'Geography', 'https://i2.wp.com/educationblog.oup.com/wp-content/uploads/2017/02/oup_22335.jpg?w=2553&ssl=1');
+INSERT INTO quizzes (creator_id, title, isPublic, category) VALUES(2, 'Intermediate Quiz', FALSE, 'Astronomy', 'https://astronomy.com/-/media/Images/News%20and%20Observing/News/2020/09/Outlooknbehqb2r.png?mw=600');
+INSERT INTO quizzes (creator_id, title, isPublic, category) VALUES(1, 'Super Hard Quiz", TRUE, 'Math', 'https://bhi61nm2cr3mkdgk1dtaov18-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/beautiful-math-1.jpg');
 
