5996a847ebe756be663d24346ed0a8a626466a3e
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-26 23:50:18 +0000

    data for users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password) VALUES ('Alice James', 'alice.james@gmail.com', 'password' );
+INSERT INTO users (name) VALUES ('Kira Johnson', 'kira.johnson@gmail.com', 'password');
+INSERT INTO users (name) VALUES ('Frank Frank', 'frank.frank@gmail.com', 'password');
