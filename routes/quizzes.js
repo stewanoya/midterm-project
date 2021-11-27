4dd492eed3821bede99771305f9157c042c7b8a6
stew anoya <stewartanoya@gmail.com>
2021-11-27 00:47:40 +0000

    fix typo

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -8,13 +8,23 @@ const router = express.Router();
 const allQuizzes = (db) => {
   router.get("/", (req, res) => {
     //should change this to only show ACTIVE quizzes
-    db.query(`SELECT * from quizzes;`).then((data) => {
-      const quizzes = data.rows;
-      console.log("THIS SHOULD SHOW ALL TEST QUIZZES", quizzes);
-      return quizzes;
-      //this for some reason, replaces index with JSON data with the quiz, got it from andy's lecture
-      // res.json({ quizzes });
-    });
+    db.query(`SELECT * from quizzes;`)
+      .then((data) => {
+        const quizzes = data.rows;
+        console.log("THIS SHOULD SHOW ALL TEST QUIZZES", quizzes);
+        return quizzes;
+        //this for some reason, replaces index with JSON data with the quiz, got it from andy's lecture
+        // res.json({ quizzes });
+      })
+      .then((quizzes) => {
+        const templateVars = { quizzes };
+        console.log("here is templateVars", templateVars);
+        res.render("index", templateVars);
+      })
+      .catch((err) => {
+        console.log("this is the error", err.message);
+        return err.message;
+      });
   });
 
   return router;
--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const allQuizzes = require("./routes/quizzes");
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
-app.use("/quizzes/", allQuizzes(db));
+app.use("/quizzes", allQuizzes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
