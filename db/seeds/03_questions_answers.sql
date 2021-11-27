1c0008ff7f3e90da27976b74132e789e71271959
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-27 00:03:29 +0000

    fixing errors

--- db/seeds/03_questions_answers.sql
+++ db/seeds/03_questions_answers.sql
@@ -1,6 +1,6 @@
-INSERT INTO questions_and_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
 VALUES (1, 'Which country has the longest coastline in the world', 'Canada', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Canada', 'Peru', 'Mexico', 'Portugal');
-INSERT INTO questions_and_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
 VALUES (2, 'How many planets are in our solar system?', '9', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', '9', '4', '3', '4');
-INSERT INTO questions_and_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
 VALUES (2, '2 + 2 = ?', '4', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', '4', '1', '3', '2');
