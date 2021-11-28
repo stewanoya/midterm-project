c11d3f656ed68a6d20530acb878e14ba657e8fae
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-28 23:23:00 +0000

    fixed some of the data

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -2,5 +2,5 @@ INSERT INTO quizzes (creator_id, title, isPublic, category, cover_image_url) VAL
 INSERT INTO quizzes (creator_id, title, isPublic, category, cover_image_url) VALUES(2, 'Intermediate Quiz', TRUE, 'Astronomy', 'https://astronomy.com/-/media/Images/News%20and%20Observing/News/2020/09/Outlooknbehqb2r.png?mw=600');
 INSERT INTO quizzes (creator_id, title, isPublic, category,cover_image_url) VALUES(1, 'Super Hard Quiz', TRUE, 'Mathematics', 'https://bhi61nm2cr3mkdgk1dtaov18-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/beautiful-math-1.jpg');
 INSERT INTO quizzes (creator_id, title, isPublic, category, cover_image_url) VALUES(4, 'EASY Quiz', TRUE, 'General', 'https://leverageedu.com/blog/wp-content/uploads/2020/02/General-Knowledge-for-Kids.jpg');
-INSERT INTO quizzes (creator_id, title, isPublic, category,cover_image_url) VALUES(1, 'Quiz', TRUE, 'Biology', 'https://www.cshl.edu/how-to-outwit-evolution/');
+INSERT INTO quizzes (creator_id, title, isPublic, category,cover_image_url) VALUES(5, 'Quiz', TRUE, 'Biology', 'https://www.thoughtco.com/thmb/gOEbyq2I6Wwh3CB1KR1_5zCr-9k=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/3-D_DNA-56a09ae45f9b58eba4b20266.jpg');
 
--- db/seeds/03_questions_answers.sql
+++ db/seeds/03_questions_answers.sql
@@ -7,4 +7,4 @@ VALUES (3, '2 + 2 = ?', '4', 'https://previews.123rf.com/images/winyu/winyu1609/
 INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
 VALUES (4, 'Who is the Prime Minister of Canada', 'Justin Trudeau', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Timothy Chalamet', 'Steven Harper', 'Justin Trudeau', 'Andrew Yang');
 INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
-VALUES (5, 'What does ATP stand for', 'adenosine triphosphate', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Advanced Turboprop', 'Association of Tennis Professionals', 'Adenosine Triphosphate', 'Automatic Train Protection');
+VALUES (5, 'What does ATP stand for', 'Adenosine Triphosphate', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Advanced Turboprop', 'Association of Tennis Professionals', 'Adenosine Triphosphate', 'Automatic Train Protection');
