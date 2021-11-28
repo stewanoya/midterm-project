93e51486952659f05e5ab118ef47919080ec89c6
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-28 23:18:33 +0000

    added more data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,3 +2,7 @@
 INSERT INTO users (name, email, password) VALUES ('Alice James', 'alice.james@gmail.com', 'password' );
 INSERT INTO users (name, email, password) VALUES ('Kira Johnson', 'kira.johnson@gmail.com', 'password');
 INSERT INTO users (name, email, password) VALUES ('Frank Frank', 'frank.frank@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Tina Johnson', 'tina.johnson@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Paul Smith', 'paul.smith@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Timothy Chalamet', 'tims@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Michael Jordan', 'jordan@gmail.com', 'password');
--- db/seeds/03_questions_answers.sql
+++ db/seeds/03_questions_answers.sql
@@ -4,3 +4,7 @@ INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, c
 VALUES (2, 'How many planets are in our solar system?', '9', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', '9', '4', '3', '4');
 INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
 VALUES (3, '2 + 2 = ?', '4', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', '4', '1', '3', '2');
+INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (4, 'Who is the Prime Minister of Canada', 'Justin Trudeau', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Timothy Chalamet', 'Steven Harper', 'Justin Trudeau', 'Andrew Yang');
+INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (5, 'What does ATP stand for', 'adenosine triphosphate', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Advanced Turboprop', 'Association of Tennis Professionals', 'Adenosine Triphosphate', 'Automatic Train Protection');
