fc78946c10d8b315782fa830c01e3d07417e5836
Kenneth Chen <kenchen0606@gmail.com>
2021-11-28 04:13:20 +0000

    button can add more question to the from

--- public/scripts/new-quiz.js
+++ public/scripts/new-quiz.js
@@ -1,33 +1,49 @@
-const loadQuestions = function() {
-  const $questions = createTweetElement();
+const loadQuestions = function(questionNum) {
+  const $questions = createTweetElement(questionNum);
   $('#question-container').append($questions);
 };
 
-const createTweetElement = function() {
+const createTweetElement = function(num) {
   let $questions = `
-    <article class="box">
-      <header class="user">
-        <div class="question">
-          <a>Hekko</a>
-        </div>
-        <a class="userhandle">@h</a>
-      </header>
-      <p>text</p>
-      <footer>
-        <a>time</a>
-        </div>
-      </footer>
+    <article class="q${num}-box">
+      <label for="q${num}-question">Title: </label>
+      <input type="text" id="q${num}-question" name="q${num}-question" required><br>
+
+      <label for="q${num}-image_url">Image URL: </label>
+      <input type="text" id="q${num}-image_url" name="q${num}-image_url"><br>
+
+      <label for="q${num}-answer">Answer: </label>
+      <select id="q${num}-answer" name="q${num}-answer">
+        <option value="a">A</option>
+        <option value="b">B</option>
+        <option value="c">C</option>
+        <option value="d">D</option>
+      </select><br>
+
+      <label for="q${num}-choice1">A: </label>
+      <input type="text" id="q${num}-choice1" name="q${num}-choice1" required><br>
+      <label for="q${num}-choice2">B: </label>
+      <input type="text" id="q${num}-choice2" name="q${num}-choice2" required><br>
+
+      <label for="q${num}-choice3">C: </label>
+      <input type="text" id="q${num}-choice3" name="q${num}-choice3" required><br>
+      <label for="q${num}-choice4">D: </label>
+      <input type="text" id="q${num}-choice4" name="q${num}-choice4" required><br><br>
     </article>
   `;
   return $questions;
 };
 
 $(document).ready(function() {
-  loadQuestions();
+  let questionNum = 1;
+
+  loadQuestions(questionNum);
+  questionNum++;
 
   $("#Add-more").submit(function(event) {
     event.preventDefault();
-    loadQuestions();
-    console.log('heelo from new quiz');
+    loadQuestions(questionNum);
+    questionNum++;
+    //console.log('heelo from new quiz');
   });
 });
