bfe57b227dc027f053c824c2bd7015dc1e234ab8
Kenneth Chen <kenchen0606@gmail.com>
2021-11-28 03:08:26 +0000

    2nd button append html

--- public/scripts/new-quiz.js
+++ public/scripts/new-quiz.js
@@ -1,8 +1,33 @@
+const loadQuestions = function() {
+  const $questions = createTweetElement();
+  $('#question-container').append($questions);
+};
 
+const createTweetElement = function() {
+  let $questions = `
+    <article class="box">
+      <header class="user">
+        <div class="question">
+          <a>Hekko</a>
+        </div>
+        <a class="userhandle">@h</a>
+      </header>
+      <p>text</p>
+      <footer>
+        <a>time</a>
+        </div>
+      </footer>
+    </article>
+  `;
+  return $questions;
+};
 
 $(document).ready(function() {
+  loadQuestions();
+
   $("#Add-more").submit(function(event) {
     event.preventDefault();
+    loadQuestions();
     console.log('heelo from new quiz');
   });
 });
