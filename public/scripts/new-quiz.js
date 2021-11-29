84b9ad12de6e82369587b682639bfbb066cd67c1
Kenneth Chen <kenchen0606@gmail.com>
2021-11-29 04:37:37 +0000

    started the quiz from layout

--- public/scripts/new-quiz.js
+++ public/scripts/new-quiz.js
@@ -6,29 +6,33 @@ const loadQuestions = function(questionNum) {
 const createQuestionElement = function(num) {
   let $questions = `
     <article class="q${num}-box">
-      <label for="q${num}-question">Title: </label>
-      <input type="text" id="q${num}-question" name="q${num}-question" required><br>
+      <div class='info'>
+        <label for="q${num}-question">Question ${num}: </label>
+        <input type="text" id="q${num}-question" name="q${num}-question" required>
 
-      <label for="q${num}-image_url">Image URL: </label>
-      <input type="text" id="q${num}-image_url" name="q${num}-image_url"><br>
+        <label for="q${num}-image_url">Image URL: </label>
+        <input type="text" id="q${num}-image_url" name="q${num}-image_url">
 
-      <label for="q${num}-answer">Answer: </label>
-      <select id="q${num}-answer" name="q${num}-answer">
-        <option value="a">A</option>
-        <option value="b">B</option>
-        <option value="c">C</option>
-        <option value="d">D</option>
-      </select><br>
+        <label for="q${num}-answer">Answer: </label>
+        <select id="q${num}-answer" name="q${num}-answer">
+          <option value="a">A</option>
+          <option value="b">B</option>
+          <option value="c">C</option>
+          <option value="d">D</option>
+        </select>
+      </div>
 
-      <label for="q${num}-choice1">A: </label>
-      <input type="text" id="q${num}-choice1" name="q${num}-choice1" required><br>
-      <label for="q${num}-choice2">B: </label>
-      <input type="text" id="q${num}-choice2" name="q${num}-choice2" required><br>
+      <div class='choices'>
+        <label for="q${num}-choice1">A: </label>
+        <input type="text" id="q${num}-choice1" name="q${num}-choice1" required><br>
+        <label for="q${num}-choice2">B: </label>
+        <input type="text" id="q${num}-choice2" name="q${num}-choice2" required><br>
 
-      <label for="q${num}-choice3">C: </label>
-      <input type="text" id="q${num}-choice3" name="q${num}-choice3"><br>
-      <label for="q${num}-choice4">D: </label>
-      <input type="text" id="q${num}-choice4" name="q${num}-choice4"><br><br>
+        <label for="q${num}-choice3">C: </label>
+        <input type="text" id="q${num}-choice3" name="q${num}-choice3"><br>
+        <label for="q${num}-choice4">D: </label>
+        <input type="text" id="q${num}-choice4" name="q${num}-choice4"><br><br>
+      </div>
     </article>
   `;
   return $questions;
