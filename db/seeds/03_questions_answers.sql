125e2da974a96d32708d69e71aaa8e657cf5c698
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-29 23:35:04 +0000

    data for question numbers

--- db/seeds/03_questions_answers.sql
+++ db/seeds/03_questions_answers.sql
@@ -1,28 +1,28 @@
-INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
-VALUES (1, 'Which country has the longest coastline in the world', 'Canada', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Peru', 'Mexico', 'Portugal', 'Canada');
-INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
-VALUES (1, 'What is the smallest country in the world?', 'Vatican City', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Monaco', 'Vatican City', 'Tuvalu', 'Slovakia');
-INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
-VALUES (1, 'Kuala Lumpur is the capital of which country?', 'Malaysia', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Malaysia', 'Singapore', 'Indonesia', 'Thailand');
+INSERT INTO questions_answers (quiz_id, question, question_number, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (1, 'Which country has the longest coastline in the world', 1, 'Canada', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Peru', 'Mexico', 'Portugal', 'Canada');
+INSERT INTO questions_answers (quiz_id, question, question_number, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (1, 'What is the smallest country in the world?', 2, 'Vatican City', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Monaco', 'Vatican City', 'Tuvalu', 'Slovakia');
+INSERT INTO questions_answers (quiz_id, question, question_number, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (1, 'Kuala Lumpur is the capital of which country?', 3, 'Malaysia', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Malaysia', 'Singapore', 'Indonesia', 'Thailand');
 
 
 
 
-INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
-VALUES (2, 'How many planets are in our solar system?', '9', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', '21', '9', '3', '4');
-INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
-VALUES (2, 'What is the smallest planet in the solar system by mass?', 'Mercury', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Saturn', 'Mars', 'Earth', 'Mercury');
-INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
-VALUES (2, 'What makes a planet a dwarf planet?', 'Size and Shape', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Colour', 'Size and Shape', 'Distance from Sun', 'Brightness');
+INSERT INTO questions_answers (quiz_id, question, question_number, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (2, 'How many planets are in our solar system?', 1, '9', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', '21', '9', '3', '4');
+INSERT INTO questions_answers (quiz_id, question, question_number, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (2, 'What is the smallest planet in the solar system by mass?', 2, 'Mercury', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Saturn', 'Mars', 'Earth', 'Mercury');
+INSERT INTO questions_answers (quiz_id, question, question_number, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (2, 'What makes a planet a dwarf planet?', 3, 'Size and Shape', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Colour', 'Size and Shape', 'Distance from Sun', 'Brightness');
 
 
 
 
-INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
-VALUES (3, 'Find the product of 72 × 3', '216', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', '7230', '216', '106', '372');
-INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
-VALUES (4, 'Who is the Prime Minister of Canada', 'Justin Trudeau', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Timothy Chalamet', 'Steven Harper', 'Justin Trudeau', 'Andrew Yang');
-INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
-VALUES (5, 'What does ATP stand for', 'Adenosine Triphosphate', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Advanced Turboprop', 'Adenosine Triphosphate', 'Association of Tennis Professionals', 'Automatic Train Protection');
-INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
-VALUES (6, 'The symbol Sb stands for stibnum or stibnite. What is the modern name of this element?', 'Antimony', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Antimony', 'Tin', 'Samarium', 'Arsenic');
+INSERT INTO questions_answers (quiz_id, question, question_number, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (3, 'Find the product of 72 × 3', 1, '216', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', '7230', '216', '106', '372');
+INSERT INTO questions_answers (quiz_id, question, question_number, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (4, 'Who is the Prime Minister of Canada', 1, 'Justin Trudeau', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Timothy Chalamet', 'Steven Harper', 'Justin Trudeau', 'Andrew Yang');
+INSERT INTO questions_answers (quiz_id, question, question_number, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (5, 'What does ATP stand for', 1, 'Adenosine Triphosphate', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Advanced Turboprop', 'Adenosine Triphosphate', 'Association of Tennis Professionals', 'Automatic Train Protection');
+INSERT INTO questions_answers (quiz_id, question, question_number, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (6, 'The symbol Sb stands for stibnum or stibnite. What is the modern name of this element?', 1, 'Antimony', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Antimony', 'Tin', 'Samarium', 'Arsenic');
