874770e37b9d76b2cf2caac984ec049568652ae3
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-29 20:08:13 +0000

    working on disabling buttons when answer is chosen and also displaying question on next page

--- db/seeds/03_questions_answers.sql
+++ db/seeds/03_questions_answers.sql
@@ -1,7 +1,23 @@
 INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
 VALUES (1, 'Which country has the longest coastline in the world', 'Canada', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Peru', 'Mexico', 'Portugal', 'Canada');
 INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (1, 'What is the smallest country in the world?', 'Vatican City', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Monaco', 'Vatican City', 'Tuvalu', 'Slovakia');
+INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (1, 'Kuala Lumpur is the capital of which country?', 'Malaysia', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Malaysia', 'Singapore', 'Indonesia', 'Thailand');
+
+
+
+
+INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
 VALUES (2, 'How many planets are in our solar system?', '9', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', '21', '9', '3', '4');
+INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (2, 'What is the smallest planet in the solar system by mass?', 'Mercury', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Saturn', 'Mars', 'Earth', 'Mercury');
+INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
+VALUES (2, 'What makes a planet a dwarf planet?', 'Size and Shape', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', 'Colour', 'Size and Shape', 'Distance from Sun', 'Brightness');
+
+
+
+
 INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
 VALUES (3, 'Find the product of 72 × 3', '216', 'https://previews.123rf.com/images/winyu/winyu1609/winyu160900148/62699053-transparent-question-marks.jpg', '7230', '216', '106', '372');
 INSERT INTO questions_answers (quiz_id, question, answer, image_url, choice_1, choice_2, choice_3, choice_4)
