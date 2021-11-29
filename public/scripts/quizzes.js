b3429579a987f70d973aa1989c4dbc4f267f98fc
stew anoya <stewartanoya@gmail.com>
2021-11-29 13:38:37 +0000

    Fix merge error

--- public/scripts/quizzes.js
+++ public/scripts/quizzes.js
@@ -1,25 +1,20 @@
-$(function() {
-$(".answers-quiz").on('click', function(){
-  const x =  $(this).text()
-  const answer = $("#correct-answer").val()
+$(function () {
+  $(".answers-quiz").on("click", function () {
+    const x = $(this).text();
+    const answer = $("#correct-answer").val();
 
-  console.log(x, answer);
-  if (x == answer){
-    // $(".answers-quiz").css({
-    //   backgroundColor: "red"
-    // })
-    $(this).css({
-      backgroundColor: "lime"
-    })
-
-
-  } else {
-    $(this).css({
-      backgroundColor: "red"
-    })
-  }
-
-
-})
-
-})
+    console.log(x, answer);
+    if (x == answer) {
+      // $(".answers-quiz").css({
+      //   backgroundColor: "red"
+      // })
+      $(this).css({
+        backgroundColor: "lime",
+      });
+    } else {
+      $(this).css({
+        backgroundColor: "red",
+      });
+    }
+  });
+});
