ca516ac12c7627f40f4e7c4a2199b357327a5f9b
stew anoya <stewartanoya@gmail.com>
2021-11-29 20:15:03 +0000

    add password check to login route

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const emailCheck = require("../helpers/emailCheck.js");
+const passCheck = require("../helpers/passwordCheck");
 const bcrypt = require("bcryptjs");
 const cookieSession = require("cookie-session");
 const app = express();
@@ -24,6 +25,28 @@ const login = (db) => {
       const templateVars = { session };
       res.render("login", templateVars);
     }
+  });
+
+  router.post("/", (req, res) => {
+    const userEmail = req.body.email;
+    const enteredPassword = req.body.password;
+
+    emailCheck(userEmail, db)
+      .then((result) => {
+        if (!result) {
+          res.send("email not found");
+        }
+        return result;
+      })
+      .then((result) => {
+        const correctPass = passCheck(enteredPassword, result.password);
+        if (correctPass) {
+          req.session.id = result.id;
+          res.redirect("/");
+        } else {
+          res.send("password incorrect");
+        }
+      });
   });
 
   return router;
