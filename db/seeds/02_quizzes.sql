2787c72e31939d79cdb59aba026b1b852aba5a19
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-30 03:38:36 +0000

    added more data

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,5 +1,7 @@
+
+
 INSERT INTO quizzes (creator_id, title, short_url, isPublic, category, cover_image_url)
-  VALUES(1, 'Super fun awesome quiz', '43ugcc0f', TRUE, 'Geography', 'https://i2.wp.com/educationblog.oup.com/wp-content/uploads/2017/02/oup_22335.jpg?w=2553&ssl=1');
+  VALUES(1, 'Super Fun Quiz', '43ugcc0f', TRUE, 'Geography', 'https://i2.wp.com/educationblog.oup.com/wp-content/uploads/2017/02/oup_22335.jpg?w=2553&ssl=1');
 
 INSERT INTO quizzes (creator_id, title, short_url, isPublic, category, cover_image_url)
   VALUES(2, 'Intermediate Quiz', '62jll9f9', TRUE, 'Astronomy', 'https://astronomy.com/-/media/Images/News%20and%20Observing/News/2020/09/Outlooknbehqb2r.png?mw=600');
@@ -11,8 +13,7 @@ INSERT INTO quizzes (creator_id, title, short_url, isPublic, category, cover_ima
   VALUES(4, 'Easy Quiz', 'cpu5pl7r', TRUE, 'General', 'https://leverageedu.com/blog/wp-content/uploads/2020/02/General-Knowledge-for-Kids.jpg');
 
 INSERT INTO quizzes (creator_id, title, short_url, isPublic, category,cover_image_url)
-  VALUES(5, 'Easy Quiz', 'xxd5payq', TRUE, 'Biology', 'https://www.thoughtco.com/thmb/gOEbyq2I6Wwh3CB1KR1_5zCr-9k=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/3-D_DNA-56a09ae45f9b58eba4b20266.jpg');
+  VALUES(5, 'Hard Quiz', 'xxd5payq', TRUE, 'Biology', 'https://www.thoughtco.com/thmb/gOEbyq2I6Wwh3CB1KR1_5zCr-9k=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/3-D_DNA-56a09ae45f9b58eba4b20266.jpg');
 
 INSERT INTO quizzes (creator_id, title, short_url, isPublic, category,cover_image_url)
   VALUES(6, 'Hard Quiz', 'e0v3c0ug', TRUE, 'Chemistry', 'https://res.cloudinary.com/grand-canyon-university/image/fetch/w_750,h_564,c_fill,g_faces,q_auto,f_auto/https://www.gcu.edu/sites/default/files/media/images/Blog/engineering-and-technology/chemistry-materials.jpg');
-
