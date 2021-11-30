5e606a7618ac8e0dffc102600208060ff06af0fa
stew anoya <stewartanoya@gmail.com>
2021-11-30 02:39:48 +0000

    fix typo

--- routes/my-quizzes.js
+++ routes/my-quizzes.js
@@ -1,15 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const cookieSession = require("cookie-session");
-const app = express();
 
-app.use(
-  cookieSession({
-    name: "session",
-    // shoutout to anyone that gets the reference
-    keys: ["The Temp at Night."],
-  })
-);
 
 const myQuizzes = (db) => {
   router.get("/", (req, res) => {
