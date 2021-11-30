27252b4b8a027a337517d1a44735cd51212fbb1e
Ansaar <Ansaar.ahmed245@gmail.com>
2021-11-30 01:48:58 +0000

    debugging

--- public/scripts/quizzes.js
+++ public/scripts/quizzes.js
@@ -1,63 +1,41 @@
-<<<<<<< HEAD
 $(function() {
-$(".answers-quiz").on('click', function(){
-  const choice =  $(this).text()
-  const answer = $(this).siblings(".correct-answer").val()
-
-  $(this).siblings(".answers-quiz").off("click");
-
-
-  if (choice == answer){
-
-    $(this).css({
-      backgroundColor: "lime"
-    })
-
-
-  } else {
-    $(this).css({
-      backgroundColor: "red"
-    })
-
-  }
-})
-})
-
-
-
-// $(function() {
-//   $(".next-btn").on('click', function(){
-//     const choice =  $(this).text()
-//     const answer = $(this).siblings(".correct-answer").val()
-
-//     $(this).siblings(".answers-quiz").off("click");
-
-//     questionid =
-
-
-
-
-
-
-//   })
-//   })
-=======
-$(function () {
-  $(".answers-quiz").on("click", function () {
-    const choice = $(this).text();
-    const answer = $(this).siblings(".correct-answer").val();
+  $(".answers-quiz").on('click', function(){
+    const choice =  $(this).text()
+    const answer = $(this).siblings(".correct-answer").val()
 
     $(this).siblings(".answers-quiz").off("click");
 
-    if (choice == answer) {
+
+    if (choice == answer){
+
       $(this).css({
-        backgroundColor: "lime",
-      });
+        backgroundColor: "lime"
+      })
+
+
     } else {
       $(this).css({
-        backgroundColor: "red",
-      });
+        backgroundColor: "red"
+      })
+
     }
-  });
-});
->>>>>>> 5a365ef398c33cbf780a8b0005fad51326494a32
+  })
+  })
+
+
+
+  // $(function() {
+  //   $(".next-btn").on('click', function(){
+  //     const choice =  $(this).text()
+  //     const answer = $(this).siblings(".correct-answer").val()
+
+  //     $(this).siblings(".answers-quiz").off("click");
+
+  //     questionid =
+
+
+
+
+
+
+  //   })
