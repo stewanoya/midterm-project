c450195bad0e1622b60cca3740acb36364465aaf
stew anoya <stewartanoya@gmail.com>
2021-11-30 13:40:01 +0000

    add animation to search button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,7 @@
 // Client facing scripts here
+$(document).ready(function () {
+  $(`.search-button`).one("click", function (e) {
+    e.preventDefault();
+    $(`#search`).css("width", "80%");
+  });
+});
