53fe2e5dafb5d8724fb089a76f4173cc87743be4
stew anoya <stewartanoya@gmail.com>
2021-11-30 01:58:05 +0000

    Add get function & db query for my-quizzes

--- routes/my-quizzes.js
+++ routes/my-quizzes.js
@@ -14,8 +14,21 @@ app.use(
 const myQuizzes = (db) => {
   router.get("/", (req, res) => {
     const session = req.session.id;
-    const templateVars = { session };
-    res.render("my-quizzes", templateVars);
+
+    if (!session) {
+      res.redirect(304, "/login");
+    }
+
+    const queryString = `SELECT * FROM users
+    JOIN quizzes ON users.id = creator_id
+    WHERE users.id = 1;`;
+    const queryValues = [session];
+
+    return db.query(queryString).then((data) => {
+      const quizzes = data.rows;
+      const templateVars = { session, quizzes };
+      res.render("my-quizzes", templateVars);
+    });
   });
 
   return router;
