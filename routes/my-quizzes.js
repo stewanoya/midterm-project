820fb7659614f1b4ca6d60d1e816154e20c25bc6
stew anoya <stewartanoya@gmail.com>
2021-11-30 03:01:34 +0000

    Fix query values

--- routes/my-quizzes.js
+++ routes/my-quizzes.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-
 const myQuizzes = (db) => {
   router.get("/", (req, res) => {
     const session = req.session.id;
 
     if (!session) {
-      res.redirect(304, "/login");
+      res.redirect("/login");
     }
 
     const queryString = `SELECT * FROM users
     JOIN quizzes ON users.id = creator_id
-    WHERE users.id = 1;`;
+    WHERE users.id = $1;`;
     const queryValues = [session];
 
-    return db.query(queryString).then((data) => {
+    return db.query(queryString, queryValues).then((data) => {
       const quizzes = data.rows;
       const templateVars = { session, quizzes };
       res.render("my-quizzes", templateVars);
