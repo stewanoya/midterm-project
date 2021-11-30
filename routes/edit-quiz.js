dce940d68882a4f08890fb88f6e6772d84f853c2
stew anoya <stewartanoya@gmail.com>
2021-11-30 19:48:15 +0000

    Create function and views for editing quiz

---
+++ routes/edit-quiz.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+const editQuiz = (db) => {
+  // the get request loads the page with the proper questions to edit
+  router.get("/:id", (req, res) => {
+    const session = req.session.id;
+
+    //grabbing just the cover image for the entire quiz, not sure if image in question_answers is supposed to be different per question.
+    const queryString = `SELECT quizzes.*, question_number, question, answer, choice_1, choice_2, choice_3, choice_4
+    FROM quizzes
+    JOIN questions_answers ON quizzes.id = quiz_id
+    WHERE quizzes.id = $1;`;
+    const editQuizID = req.params.id;
+    const queryValues = [editQuizID];
+
+    return db.query(queryString, queryValues).then((data) => {
+      const quizzes = data.rows;
+      const templateVars = { session, quizzes };
+      res.render("edit-quiz", templateVars);
+    });
+  });
+
+  return router;
+};
+
+module.exports = editQuiz;
