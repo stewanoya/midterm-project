77ef07ed18765fd2baf7c16cf3da42542b382cc0
stew anoya <stewartanoya@gmail.com>
2021-11-30 13:44:35 +0000

    Fix typo

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,7 @@
 $(document).ready(function () {
   $(`.search-button`).one("click", function (e) {
     e.preventDefault();
+    $(this).unbind(e);
     $(`#search`).css("width", "80%");
   });
 });
