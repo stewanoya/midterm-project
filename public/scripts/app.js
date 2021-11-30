51310713bd161366c64b3d846f533ef874bd87cf
stew anoya <stewartanoya@gmail.com>
2021-11-30 13:47:25 +0000

    refactor searchAnimation function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,12 @@
 // Client facing scripts here
 $(document).ready(function () {
+  searchAnimation();
+});
+
+const searchAnimation = () => {
   $(`.search-button`).one("click", function (e) {
     e.preventDefault();
     $(this).unbind(e);
     $(`#search`).css("width", "80%");
   });
-});
+};
