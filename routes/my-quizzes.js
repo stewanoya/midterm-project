8beed3d0291020022b8b933fa0a91c68a2ed0656
stew anoya <stewartanoya@gmail.com>
2021-11-30 15:28:02 +0000

    Fix query

--- routes/my-quizzes.js
+++ routes/my-quizzes.js
@@ -9,13 +9,16 @@ const myQuizzes = (db) => {
       res.status(304).redirect("/login");
     }
 
-    const queryString = `SELECT * FROM users
-    JOIN quizzes ON users.id = creator_id
+    const queryString = `SELECT title, category, created_at::date, ispublic, cover_image_url, quizzes.creator_id as creator
+    FROM quizzes
+    JOIN users ON users.id = creator_id
     WHERE users.id = $1;`;
     const queryValues = [session];
 
     return db.query(queryString, queryValues).then((data) => {
       const quizzes = data.rows;
+      console.log(quizzes);
+
       const templateVars = { session, quizzes };
       res.render("my-quizzes", templateVars);
     });
