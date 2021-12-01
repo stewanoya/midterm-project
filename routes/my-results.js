ad6b8f72ebce7fe94e5345dc8083ef5a5f77b3fb
stew anoya <stewartanoya@gmail.com>
2021-12-01 20:48:32 +0000

    change query to include question count '

--- routes/my-results.js
+++ routes/my-results.js
@@ -9,15 +9,19 @@ const myResults = (db) => {
       res.status(304).redirect("/login");
     }
 
-    const queryString = `SELECT name, score, title, cover_image_url, category, finish_at as date_completed, quizzes.short_url
+    const queryString = `SELECT name, score, title, cover_image_url, category, finish_at as date_completed, quizzes.short_url, COUNT(questions_answers.id) as count
     FROM users
     JOIN results ON users.id = user_id
     JOIN quizzes on quizzes.id = quiz_id
-    WHERE users.id = $1;`;
+    JOIN questions_answers on quizzes.id = questions_answers.quiz_id
+    WHERE users.id = $1
+    GROUP BY users.name, results.score, quizzes.title, quizzes.cover_image_url, quizzes.category, results.finish_at, quizzes.short_url;`;
+
     const queryValues = [session];
 
     return db.query(queryString, queryValues).then((data) => {
       const quizzes = data.rows;
+      console.log("data from results page", data.rows);
       const templateVars = { session, quizzes };
       res.render("my-results", templateVars);
     });
--- routes/result-share.js
+++ routes/result-share.js
@@ -4,8 +4,24 @@ const router = express.Router();
 const resultShare = (db) => {
   router.get("/:userID/:shortURL", (req, res) => {
     const session = req.session.id;
-    const templateVars = { session };
-    res.render("result-share", templateVars);
+    const shortURL = req.params.shortURL;
+
+    const queryString = `SELECT name, score, title, cover_image_url, category, finish_at as date_completed, quizzes.short_url, COUNT(questions_answers.id) as count
+    FROM users
+    JOIN results ON users.id = user_id
+    JOIN quizzes on quizzes.id = quiz_id
+    JOIN questions_answers on quizzes.id = questions_answers.quiz_id
+    WHERE users.id = $1
+    AND quizzes.short_url = $2
+    GROUP BY users.name, results.score, quizzes.title, quizzes.cover_image_url, quizzes.category, results.finish_at, quizzes.short_url;`;
+
+    const queryValues = [session, shortURL];
+
+    return db.query(queryString, queryValues).then((data) => {
+      const result = data.rows;
+      const templateVars = { session, result };
+      res.render("result-share", templateVars);
+    });
   });
 
   return router;
