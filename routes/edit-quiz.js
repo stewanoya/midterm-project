fae18ebabbe6a408135a61b1043df331479f9197
stew anoya <stewartanoya@gmail.com>
2021-12-01 17:00:26 +0000

    Fix edit logic for answers while displaying default answer

--- routes/edit-quiz.js
+++ routes/edit-quiz.js
@@ -6,7 +6,7 @@ const editQuiz = (db) => {
   router.get("/:id", (req, res) => {
     const session = req.session.id;
     //grabbing just the cover image for the entire quiz, not sure if image in question_answers is supposed to be different per question.
-    const queryString = `SELECT quizzes.id, quizzes.cover_image_url, quizzes.title, question_number, question, answer, choice_1, choice_2, choice_3, choice_4, questions_answers.id as questionID, image_url, ispublic
+    const queryString = `SELECT quizzes.id, quizzes.cover_image_url, quizzes.title, quizzes.category, question_number, question, answer, choice_1, choice_2, choice_3, choice_4, questions_answers.id as questionID, image_url, ispublic
     FROM quizzes
     JOIN questions_answers ON quizzes.id = quiz_id
     WHERE quizzes.id = $1;`;
@@ -33,59 +33,76 @@ const editQuiz = (db) => {
       isPublic = `FALSE`;
     }
 
-    const queryString = `UPDATE quizzes
+    let queryString = `UPDATE quizzes
     SET title = $1,
     cover_image_url = $2,
-    ispublic = $3
-    WHERE quizzes.id = $4;
-    `;
+    ispublic = $3`;
+
     const queryValues = [quiz.title, quiz.cover_image_url, isPublic, quizID];
+    console.log(req.body);
+    // Will check to see if category has changed, if it has, it will add query and push value to array
+    if (req.body.category) {
+      queryString += `, category = $5`;
+      queryValues.push(quiz.category);
+    }
+
+    queryString += `WHERE quizzes.id = $4;`;
+
     return db
       .query(queryString, queryValues)
       .then(() => {
         for (let i = 0; i < req.body.question.length; i++) {
-          // will check the answer input against the choice, and set the value to the exact same as the matching choice field
           let answer = "";
-
-          if (req.body.answer[i] === "choice1") {
-            answer = req.body.choice_1[i];
-          }
-          if (req.body.answer[i] === "choice2") {
-            answer = req.body.choice_2[i];
-          }
-          if (req.body.answer[i] === "choice3") {
-            answer = req.body.choice_3[i];
-          }
-          if (req.body.answer[i] === "choice4") {
-            answer = req.body.choice_4[i];
-          }
-
           let queryString = `UPDATE questions_answers
         SET question = $1,
         image_url = $2,
         choice_1 = $3,
-        choice_2 = $4,
-        answer = $5
+        choice_2 = $4
         `;
-
-          // will check if there are more than 2 choices, and add the queries incrementally
-          if (req.body.choice_3) {
-            queryString += `, choice_3 = $7`;
-            if (req.body.choice_4) {
-              queryString += `, choice_4 = $8`;
-            }
-          }
-          queryString += `WHERE questions_answers.id = $6;`;
           const queryValues = [
             req.body.question[i],
             req.body.image_url[i],
             req.body.choice_1[i],
             req.body.choice_2[i],
-            answer,
             req.body.questionid[i],
-            req.body.choice_3[i],
-            req.body.choice_4[i],
           ];
+
+          // will check the answer input against the choice,
+          //and set the value to the exact same as the matching choice field
+          // we adjust query accordingly and push value to array
+          // if no value is passed, we assume user doesn't want to change answer
+
+          // will check if there are more than 2 choices, and add the queries incrementally along with queryValues to array
+          if (req.body.choice_3) {
+            queryString += `, choice_3 = $6`;
+            queryValues.push(req.body.choice_3[i]);
+            if (req.body.choice_4) {
+              queryString += `, choice_4 = $7`;
+              queryValues.push(req.body.choice_4[i]);
+            }
+          }
+          if (req.body.answer[i] === "choice1") {
+            answer = req.body.choice_1[i];
+            queryString += `, answer = $9`;
+            queryValues.push(answer);
+          }
+          if (req.body.answer[i] === "choice2") {
+            answer = req.body.choice_2[i];
+            queryString += `, answer = $9`;
+            queryValues.push(answer);
+          }
+          if (req.body.answer[i] === "choice3") {
+            answer = req.body.choice_3[i];
+            queryString += `, answer = $9`;
+            queryValues.push(answer);
+          }
+          if (req.body.answer[i] === "choice4") {
+            answer = req.body.choice_4[i];
+            queryString += `, answer = $9`;
+            queryValues.push(answer);
+          }
+          queryString += `WHERE questions_answers.id = $5;`;
+
           db.query(queryString, queryValues);
         }
       })
