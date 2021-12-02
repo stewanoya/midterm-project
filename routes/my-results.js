3aa64025059eb209ab6382cebc904de5c1a0d3aa
Ansaar <Ansaar.ahmed245@gmail.com>
2021-12-02 01:35:44 +0000

    modified select query for displaying score

--- routes/my-results.js
+++ routes/my-results.js
@@ -9,10 +9,6 @@ const myResults = (db) => {
       res.status(304).redirect("/login");
     }
 
-<<<<<<< HEAD
-  //   `SELECT COUNT(*)
-  // FROM questions_answers
-  // WHERE quiz_id IN (SELECT id FROM quizzes WHERE short_url =$1);`;
 
     const queryString = `SELECT results.*, quizzes.*, count(questions_answers.id) AS total
     FROM results
@@ -20,22 +16,11 @@ const myResults = (db) => {
     JOIN questions_answers ON quizzes.id = questions_answers.quiz_id
     WHERE user_id = $1
     GROUP BY results.id, quizzes.id;`;
-=======
-    const queryString = `SELECT name, score, title, cover_image_url, category, finish_at as date_completed, quizzes.short_url, COUNT(questions_answers.id) as count
-    FROM users
-    JOIN results ON users.id = user_id
-    JOIN quizzes on quizzes.id = quiz_id
-    JOIN questions_answers on quizzes.id = questions_answers.quiz_id
-    WHERE users.id = $1
-    GROUP BY users.name, results.score, quizzes.title, quizzes.cover_image_url, quizzes.category, results.finish_at, quizzes.short_url;`;
-
->>>>>>> 95f9eeb37564051a224095a9cdc11d2a734cb06a
     const queryValues = [session];
 
     return db.query(queryString, queryValues).then((data) => {
       const quizzes = data.rows;
       console.log("---->", quizzes[0].total)
-      // const total =
       const templateVars = { session, quizzes };
       res.render("my-results", templateVars);
     });
