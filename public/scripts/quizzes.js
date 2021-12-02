cc7376f05c6a9af58109aba05c6121499846bfc5
Ansaar <Ansaar.ahmed245@gmail.com>
2021-12-02 21:29:40 +0000

    cleaning up

--- public/scripts/quizzes.js
+++ public/scripts/quizzes.js
@@ -1,80 +1,26 @@
 
-// let questionsAnswered = false;
 
 $(function() {
-  $(".answers-quiz").on('click', function(){
-    const choice =  $(this).text()
-    const answer = $(this).siblings(".correct-answer").val()
+  $(".answers-quiz").on('click', function() {
+    const choice =  $(this).text();
+    const answer = $(this).siblings(".correct-answer").val();
 
     $(this).siblings(".answers-quiz").off("click");
     $("#answer").val(choice);
 
-    // questionsAnswered = true;
-
-    if (choice == answer){
+    if (choice == answer) {
 
 
       $(this).css({
         backgroundColor: "lime"
 
-      })
+      });
 
     } else {
       $(this).css({
         backgroundColor: "red"
-      })
+      });
 
     }
-  })
-  })
-
-
-//   const nextQuestion = function(shorturl, quizid, last_question){
-//     console.log($("#last-question").val());
-
-//     if (questionsAnswered === true){
-//        questionsAnswered = false;
-//        window.location.href = `/quizzes/${shorturl}?questionid=${quizid}`
-
-
-//   } else {
-//     $("#errormsg")
-//         .slideDown("slow");
-//   };
-
-// }
-
-    // $("#next_question").on('click', function()
-    // {
-
-    //   if (questionsAnswered === true){
-
-    //   }$.ajax({
-    //     method: "POST",
-    //     url:
-    //   })
-
-
-
-
-    // }
-
-
-
-
-
-
-
-  // $(function() {
-  //   $(".next-btn").on('click', function(){
-  //     const choice =  $(this).text()
-  //     const answer = $(this).siblings(".correct-answer").val()
-
-  //     $(this).siblings(".answers-quiz").off("click");
-
-  //     questionid =
-
-  //   })
-
-  // $("#submitQuiz").submit(function (event) {
-  //   event.preventDefault();
+  });
+});
