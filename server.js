7b1e258b286710b00903154235267ed2070549fb
Ansaar <Ansaar.ahmed245@gmail.com>
2021-12-02 21:28:33 +0000

    cleaning up code & refractoring

--- server.js
+++ server.js
@@ -76,6 +76,7 @@ app.use("/search", searchRoutes(db));
 app.use("/logout", logoutRoutes());
 app.use("/login", loginRoutes(db));
 app.use("/my-quizzes", myQuizzesRoutes(db));
+app.use("/quizzes", quizzesRoutes(db));
 app.use("/my-results", myResultsRoutes(db));
 app.use("/delete", deleteQuizRoutes(db));
 app.use("/edit", editQuizRoutes(db));
@@ -99,127 +100,6 @@ app.get("/", (req, res) => {
     });
 });
 
-//displaying score //
-
-app.get("/quizzes/:short_url/results", (req, res) => {
-  sqlQuery = `SELECT COUNT(*)
-  FROM questions_answers
-  WHERE quiz_id IN (SELECT id FROM quizzes WHERE short_url =$1);`;
-
-  db.query(sqlQuery, [req.params.short_url]).then((data) => {
-    const score = req.session.score;
-    const total = data.rows[0].count;
-    const templateVars = {
-      score,
-      session: req.session.id,
-      total,
-    };
-
-    res.render("quiz-results", templateVars);
-  });
-});
-
-//quiz taking and incrementing score with correct answer//
-app.post("/quizzes/:short_url", (req, res) => {
-  sqlQuery = `SELECT questions_answers.* FROM questions_answers
-  WHERE id = $1
-  ;`;
-
-  sqlQuery2 = `INSERT INTO results (short_url, score, quiz_id, user_id)
-  VALUES ($1, $2, $3, $4) RETURNING *`;
-
-  db.query(sqlQuery, [req.body.questionid])
-    .then((data) => {
-      const question = data.rows[0];
-      const answer = question.answer;
-
-      if (answer == req.body.answer) {
-        req.session.score = req.session.score + 1;
-      }
-      //if on last question redirect to results page //
-      if (req.body.last_question === "true") {
-        const user_id = req.session.id || 0;
-        db.query(sqlQuery2, [
-          req.params.short_url,
-          req.session.score,
-          req.session.quizid,
-          user_id,
-        ])
-
-          .then((data) => {
-            // console.log("???sajdkajsd", data.rows);
-            res.redirect(`/result/${data.rows[0].id}/${req.params.short_url}`);
-            return;
-          })
-          .catch((err) => {
-            res.status(500).json({ error: err.message });
-          });
-      } else {
-        //if not on last question, redirect to next question //
-
-        res.redirect(
-          `/quizzes/${req.params.short_url}?questionid=${req.body.questionid}`
-        );
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-    });
-});
-
-// get route for quiz //
-app.get("/quizzes/:short_url", (req, res) => {
-  let sqlQuery;
-  let variables;
-
-  if (req.query.questionid) {
-    sqlQuery = `SELECT quizzes.category, quizzes.short_url, quizzes.id, quizzes.title, questions_answers.*,
-      (SELECT COUNT(questions_answers.quiz_id)
-      FROM questions_answers
-      JOIN quizzes ON quizzes.id = quiz_id
-      WHERE short_url = $1) as total
-
-      FROM questions_answers
-      JOIN quizzes ON quizzes.id = quiz_id
-      WHERE short_url = $1
-      AND questions_answers.id > $2
-      ORDER BY questions_answers.id
-      LIMIT 1;`;
-
-    variables = [req.params.short_url, req.query.questionid];
-  } else {
-    req.session.score = 0;
-
-    sqlQuery = `SELECT quizzes.category, quizzes.id, quizzes.short_url, quizzes.title, questions_answers.*,
-      (SELECT COUNT(questions_answers.quiz_id)
-      FROM questions_answers
-      JOIN quizzes ON quizzes.id = quiz_id
-      WHERE short_url = $1) as total
-
-      FROM questions_answers
-      JOIN quizzes ON quizzes.id = quiz_id
-      WHERE short_url = $1
-      ORDER BY questions_answers.id
-      LIMIT 1;`;
-
-    variables = [req.params.short_url];
-  }
-
-  db.query(sqlQuery, variables)
-    .then((data) => {
-      const quiz = data.rows;
-      req.session.quizid = data.rows[0] ? data.rows[0].quiz_id : 0;
-      const session = req.session.id;
-      const templateVars = {
-        quiz,
-        session,
-      };
-      res.render("quizzes", templateVars);
-    })
-    .catch((err) => {
-      res.status(500).json({ error: err.message });
-    });
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
